a71f6205c5bdbfab861bbc79cf2e3e1353518655
John Carlo B. Alarva <jc.alarva1994@gmail.com>
2025-04-22 15:54:09 +0800

    Add files via upload

---
+++ ParentChildLocatorDropDown.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ParentChildLocatorDropDown 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.manage().window().maximize();
+		
+		//Handling dynamic dropdown with Parent to Child relationship xpath
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXTaction")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR']//a[@value='DEL']")).click(); //div(parent)//a(child)
+		Thread.sleep(2000);
+		driver.findElement(By.id("ctl00_mainContent_ddl_destinationStation1_CTXTaction")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='DXB']")).click(); //div(parent)//a(child)
+	}
+}
